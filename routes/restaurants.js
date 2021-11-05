4b86f0a894b753112fad5ffa24b35e49e68e0393
Juho Vatanen <juho.vatanen@gmail.com>
2021-11-05 19:33:45 +0200

    Restaurants add,edit,get working

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -8,7 +8,7 @@ module.exports = router
 //Return all restaurants
 router.get('/', async (req, res) => {
   const { rows } = await db.query('select * from public.restaurants')
-  res.send(rows);
+  res.json(rows);
 })
 
 //Returns details about specific restaurant by its id
@@ -17,10 +17,10 @@ router.get('/:id', async (req, res) => {
   const { rows } = await db.query('select * from public.restaurants WHERE id = $1',[id])
   if(rows.length == 0)
     return res.status(404).send("That restaurant does not exist.");
-  res.send(rows[0]);
+  res.json(rows[0]);
 })
 
-
+//Creating new restaurant
 router.post('/',auth,async(req,res)=>{
   if(!req.user.manager)
     return res.status(403).send("You are not a manager.");
@@ -31,12 +31,36 @@ router.post('/',auth,async(req,res)=>{
       res.status(400).send("All input is required");
   }
   
-  const query = await db.query('INSERT INTO public.restaurants(name, managerid, review, costlevel, tags, "imageURL", deliveryfee) VALUES ($1, $2, $3, $4, $5, $6, $7',
-  [restaurantName,req.user.id,rating,costlevel,tags,imageURL,deliveryFee]);
+  var { rowCount} = await db.query('INSERT INTO public.restaurants(name, managerid, review, costlevel, tags, "imageURL", deliveryfee) VALUES ($1, $2, $3, $4, $5, $6, $7);',[restaurantName,req.user.id,rating,costlevel,tags,imageURL,deliveryFee]);
+  // await db.query('SELECT id from public.restaurants where name = $1')
+  // console.log(query)
 
-  res.send(query);
+  return rowCount == 1 ? res.status(201).send("Success"):res.status(500).send("Something went wrong");
 })
 
+//Edit restaurant
+router.put('/:id',auth,async(req,res)=>{
+  const { id } = req.params;
+  if(!req.user.manager)
+    return res.status(403).send("You are not a manager.");
+    
+  //Auth check
+  const { rows } = await db.query('select * from public.restaurants WHERE id = $1',[id])
+  if(rows[0].managerid != req.user.id)
+    return res.status(403).send("You do not have permissions to edit this restaurant.");
+
+  var { restaurantName, rating, costlevel, tags, imageURL, deliveryFee } = req.body;
+  if (!(restaurantName && rating && costlevel && tags && imageURL &&deliveryFee)) {
+    res.status(400).send("All input is required");
+  }
+  
+  console.log("User ID: $1 RestID: $2",[req.user.id,id]);
+  var { rowCount} = await db.query('UPDATE public.restaurants	SET name=$3, review=$4, costlevel=$5, tags=$6, "imageURL"=$7, deliveryfee=$8 WHERE managerid = $2 AND id = $1;',
+  [id,req.user.id,restaurantName,rating,costlevel,tags,imageURL,deliveryFee]);
+
+  return rowCount == 1 ? res.status(201).send("Success"):res.status(500).send("Something went wrong");
+
+})
 
 
 //Delete restaurants
@@ -54,5 +78,5 @@ router.delete('/:id',auth, async (req, res) => {
     return res.status(403).send("You do not have permissions to edit other restaurants.");
 
   const result = await db.query('DELETE FROM public.restaurants WHERE id = $1 AND managerid = $2',[id,req.user.id])
-  res.send(result);
+  res.json(result);
 })
