0f13ed4e54651b72e91d256fce126380e484720e
Juho Vatanen <juho.vatanen@gmail.com>
2021-11-07 01:40:37 +0200

    Removed oopsie and changed creds

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg')
 const pool = new Pool(
   {
-    connectionString: process.env.DATABASE_URL||"postgres://rlggbbvctzjbmk:9b4d3f3e2e54ef0d8c4fc7a3d5eeedfba3b607ba7d0e172ce380083fb74dac93@ec2-54-73-58-75.eu-west-1.compute.amazonaws.com:5432/d9q5tnf8so2pmf",
+    connectionString: process.env.DATABASE_URL,
     ssl: {
           rejectUnauthorized: false
       }})
