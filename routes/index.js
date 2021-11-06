f91cc06614f0787cf3e07020a48600b6b4ce32e4
Juho Vatanen <juho.vatanen@gmail.com>
2021-11-06 16:53:47 +0200

    CORS added

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const restaurants = require('./restaurants')
 const register = require('./register');
 const login = require('./login');
 const debug = require('./debug')
+const cors = require('cors');
 module.exports = app => {
   app.use(json());
+  app.use(cors());
   app.use('/restaurants', restaurants)
   app.use('/register', register);
   app.use('/login',login);
